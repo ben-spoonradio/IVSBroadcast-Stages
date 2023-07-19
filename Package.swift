de51818f692dcb49fec2bdaf16441b7f53a44629
Ben <ben@Ben.local>
2023-07-19 11:55:48 +0900

    target name changed

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "AmazonIVSBroadcast-Stages",
+    name: "AmazonIVSBroadcast",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AmazonIVSBroadcast-Stages",
-            targets: ["AmazonIVSBroadcast-Stages"]),
+            name: "AmazonIVSBroadcast",
+            targets: ["AmazonIVSBroadcast"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "AmazonIVSBroadcast-Stages",
+            name: "AmazonIVSBroadcast",
             url: "https://github.com/ben-spoonradio/IVSBroadcast-Stages/releases/download/1.9.1.2/AmazonIVSBroadcast-Stages.xcframework.zip",
             checksum: "8b21ebe380f360a6c2b27cf7fbbfe9503a6427fa5bde8f09fb38d3327f8c8088"
         ),
